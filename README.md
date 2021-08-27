0265c73ae767bbc66b4e958cc41d1af2c88fcd8b
Piyush <piyush41y08h@gmail.com>
2021-08-27 23:24:18 +0530

    initial

---
+++ README.md
@@ -0,0 +1,9 @@
+# Dilute Acid
+
+Dilute any concentration of Acids with ease.
+
+![Demo](demo.gif, "Demo of the CLI")
+
+## Authors
+
+- [@41y08h](https://www.github.com/41y08h)
---
+++ src/main.py
@@ -0,0 +1,14 @@
+from pyfiglet import Figlet
+
+# Heading
+print(Figlet(font='slant').renderText('Dilute Acid'))
+
+
+concentration1 = (float)(input("[?] Current concentration of ACID (in %): "))
+concentration2 = (float)(input("[?] Required concentration of ACID (in %): "))
+volume = (float)(input("[?] Required volume of diluted ACID (in ml): "))
+
+acidVolumeRequired = concentration2 * volume / concentration1
+waterVolumeRequired = volume - acidVolumeRequired
+
+print(f"\n>> {round(waterVolumeRequired, 1)}ml H₂O + {round(acidVolumeRequired, 1)}ml {concentration1}% ACID = {round(volume, 1)}ml")
